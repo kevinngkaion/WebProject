cb284924d54cb6e6a67c8cfd78d4fbc05387b134
Kevin Ngkaion <kevinngkaion@gmail.com>
2022-12-03 01:39:43 -0800

    Completed ajaxGetPost for 219 website. Also added sql to 213 project

--- scripts.js
+++ scripts.js
@@ -24,3 +24,56 @@ function validateForm(){
     }
     return isValid;
 }
+
+function loadDoc() {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 1 ) {
+            document.getElementById("ready1").innerHTML =
+                "Readystate = 1 and server connection established";
+        }
+        if (this.readyState == 2 ) {
+            document.getElementById("ready2").innerHTML =
+                "Readystate = 2 and Request has been received.";
+        }
+        if (this.readyState == 3 ) {
+            document.getElementById("ready3").innerHTML =
+                "Readystate = 3 and Request is being processed.";
+        }
+        if (this.readyState == 4 ) {
+            document.getElementById("ready4").innerHTML =
+                "Readystate = 4 and Request finished and Response is ready. ";
+        }
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("demo").innerHTML =
+                this.responseText;
+        }
+    };
+    xhttp.open("GET", "ajax_info.html", true);
+    xhttp.send();
+}
+
+function postToDB() {
+    let email = document.getElementById("postEmail").value;
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange =  function() {
+        if (xhttp.readyState === 4 && xhttp.status === 200) {
+            document.getElementById("post").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("POST","AJAXdbEntry.php",true);
+    xhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    xhttp.send("&email=" + email);
+}
+
+function getFromDB() {
+    let xhttp = new XMLHttpRequest();
+    let email = document.getElementById("getEmail").value;
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState === 4 && xhttp.status === 200) {
+            document.getElementById("getResponse").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET","AJAXdbGET.php?email="+email,true);
+    xhttp.send();
+}
